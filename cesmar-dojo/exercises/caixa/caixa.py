dae580d97d813bd142ec9b896a75d4b6ffaccd17
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-11-11 14:55:28 -0200

    Add caixa exercise

---
+++ cesmar-dojo/exercises/caixa/caixa.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class CaixaEletronico():
+    """
+    * Entregar o menor número de notas;
+    * E possível sacar o valor solicitado com as notas disponíveis;
+    * Saldo do cliente infinito;
+    * Quantidade de notas infinito (pode-se colocar um valor finito de cédulas para aumentar a dificuldade do problema);
+    * Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00
+    """
+
+    def sacar(self, valor):
+        pass
+
+    def executarSaque(self, valor):
+        """ Implemente aqui o comando de saque"""
+        print("Sacando R$", valor)
+        self.sacar(valor)
+
+if __name__ == '__main__':
+    caixaEletronico = CaixaEletronico()
+    caixaEletronico.executarSaque(10)
