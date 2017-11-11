ec9419f9bba81776787d559daa3e13eb686292b8
otaviocarvalho <ocarvalh@thoughtworks.com>
2017-11-11 14:55:37 -0200

    Add fizzbuzz exercise

---
+++ cesmar-dojo/exercises/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+class FizzBuzz():
+    """
+    * Retorne as strings Fizz, Buzz ou FizzBuzz dependendo do valor da entrada;
+    * Se o valor da entrada for divisível por 3, diga Fizz;
+    * Se o valor da entrada for divisível por 5, diga Buzz;
+    * Se o valor da entrada for divisível por 3 e por 5, diga FizzBuzz;
+    * O valor de entrada é qualquer número inteiro válido;
+    """
+
+    def ehFizz(self, valor):
+        pass
+
+    def ehBuzz(self, valor):
+        pass
+
+    def ehFizzBuzz(self, valor):
+        pass
+
+    def executar(self, valor):
+        """ Implemente aqui a logica do FizzBuzz"""
+        print("Executando o FizzBuzz para o valor ", valor)
+        self.ehFizzBuzz(valor)
+
+if __name__ == '__main__':
+    fizzBuzz = FizzBuzz()
+    fizzBuzz.executar(15)
+    fizzBuzz.ehFizz(15)
---
+++ cesmar-dojo/exercises/fizzbuzz/test_fizzbuzz.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import unittest
+from fizzbuzz import FizzBuzz
+
+class FizzBuzzTest(unittest.TestCase):
+
+    def setUp(self):
+        self.numeros = list(range(100))
+        self.fizzBuzz = FizzBuzz()
+
+    def test_fizz(self):
+        """Se o número for divisível por 3, diga Fizz"""
+        self.assertTrue(self.fizzBuzz.ehFizz(self.numeros[3]))
+        self.assertTrue(self.fizzBuzz.ehFizz(self.numeros[33]))
+        self.assertTrue(self.fizzBuzz.ehFizz(self.numeros[66]))
+        self.assertFalse(self.fizzBuzz.ehFizz(self.numeros[0]))
+        self.assertFalse(self.fizzBuzz.ehFizz(self.numeros[5]))
+        self.assertFalse(self.fizzBuzz.ehFizz(self.numeros[10]))
+
+    def test_buzz(self):
+        """Se o número for divisível por 5, diga Buzz"""
+        self.assertTrue(self.fizzBuzz.ehBuzz(self.numeros[5]))
+        self.assertTrue(self.fizzBuzz.ehBuzz(self.numeros[55]))
+        self.assertTrue(self.fizzBuzz.ehBuzz(self.numeros[65]))
+        self.assertFalse(self.fizzBuzz.ehBuzz(self.numeros[3]))
+        self.assertFalse(self.fizzBuzz.ehBuzz(self.numeros[33]))
+        self.assertFalse(self.fizzBuzz.ehBuzz(self.numeros[93]))
+
+    def test_fizzBuzz(self):
+        """Se o número for divisível por 3 e por 5, diga FizzBuzz"""
+        self.assertTrue(self.fizzBuzz.ehFizzBuzz(self.numeros[15]))
+        self.assertTrue(self.fizzBuzz.ehFizzBuzz(self.numeros[30]))
+        self.assertTrue(self.fizzBuzz.ehFizzBuzz(self.numeros[60]))
+        self.assertFalse(self.fizzBuzz.ehFizzBuzz(self.numeros[5]))
+        self.assertFalse(self.fizzBuzz.ehFizzBuzz(self.numeros[10]))
+        self.assertFalse(self.fizzBuzz.ehFizzBuzz(self.numeros[100]))
+
+if __name__ == '__main__':
+    unittest.main()
